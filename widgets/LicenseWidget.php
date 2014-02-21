230405eaf626bdebed1777a20bf9eb9a374ce195
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2014-02-21 08:10:39 +0100

    Converted tabs to spaces

--- widgets/LicenseWidget.php
+++ widgets/LicenseWidget.php
@@ -13,54 +13,54 @@ namespace Contao;
 
 class LicenseWidget extends \Widget
 {
-	/**
-	 * Submit user input
-	 * @var boolean
-	 */
-	protected $blnSubmitInput = true;
+    /**
+     * Submit user input
+     * @var boolean
+     */
+    protected $blnSubmitInput = true;
 
-	/**
-	 * Template
-	 * @var string
-	 */
-	protected $strTemplate = 'be_widget';
-
-
-	/**
-	 * Add specific attributes
-	 * @param string
-	 * @param mixed
-	 */
-	public function __set($strKey, $varValue)
-	{
-		switch ($strKey)
-		{
-			case 'mandatory':
-				$this->arrConfiguration['mandatory'] = $varValue ? true : false;
-				break;
-
-			default:
-				parent::__set($strKey, $varValue);
-				break;
-		}
-	}
+    /**
+     * Template
+     * @var string
+     */
+    protected $strTemplate = 'be_widget';
 
 
-	/**
-	 * Generate the widget and return it as string
-	 * @return string
-	 */
-	public function generate()
-	{
+    /**
+     * Add specific attributes
+     * @param string
+     * @param mixed
+     */
+    public function __set($strKey, $varValue)
+    {
+        switch ($strKey)
+        {
+            case 'mandatory':
+                $this->arrConfiguration['mandatory'] = $varValue ? true : false;
+                break;
+
+            default:
+                parent::__set($strKey, $varValue);
+                break;
+        }
+    }
+
+
+    /**
+     * Generate the widget and return it as string
+     * @return string
+     */
+    public function generate()
+    {
         return sprintf('<div id="ctrl_%s" class="tl_checkbox_container%s">%s<input type="checkbox" name="%s" id="opt_%s" class="tl_checkbox" value="1"%s%s onfocus="Backend.getScrollOffset();" /> <label for="opt_%s">%s</label></div>',
-						$this->strId,
-						(strlen($this->strClass) ? ' ' . $this->strClass : ''),
-						($this->varValue ? '' : $this->license[0] . '<br />'),
-						$this->strName,
-						$this->strId,
-						(($this->varValue == 1) ? ' checked="checked"' : ''),
-						$this->getAttributes(),
-						$this->strId,
-						$this->license[1]);
-	}
+                        $this->strId,
+                        (strlen($this->strClass) ? ' ' . $this->strClass : ''),
+                        ($this->varValue ? '' : $this->license[0] . '<br />'),
+                        $this->strName,
+                        $this->strId,
+                        (($this->varValue == 1) ? ' checked="checked"' : ''),
+                        $this->getAttributes(),
+                        $this->strId,
+                        $this->license[1]);
+    }
 }
